0d5381040dd5793fd402e5708c1b0e09dc18cdcb
Mun <156924828+PillowsWannaCry@users.noreply.github.com>
2024-04-09 00:42:53 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+void DecimalToBinary(int n)
+{
+    Stack* x = new Stack();
+    while (n>0)
+    {
+        x->push(n % 2);
+        n /= 2;
+    }
+    x->printStack();
+}
+
+int main()
+{
+
+    /*Stack* s = new Stack();
+    s->push(7);
+    s->push(10);
+    s->push(18);
+    s->push(20);
+    cout << "\n";
+    s->printStack();
+    s->pop();
+    s->pop();
+    cout << "\n";
+    s->printStack();
+    s->pop();
+    s->pop();
+    cout << "\n";
+    s->printStack();*/
+    
+    DecimalToBinary(5);
+
+    return 0;
+}
 
